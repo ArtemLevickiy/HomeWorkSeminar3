0858d757953e66a2f8f35c553037d74a0918294a
Artem <rrampage1991@gmail.com>
2022-08-18 18:07:08 +0300

    Tasks 1-5

---
+++ Task_02/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива.
+
+void FillArray(int[] arrayColection)
+{
+    int index = 0;
+    while (index < arrayColection.Length)
+    {
+        arrayColection[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+void PrintArray(int[] arrayColection)
+{
+    int index = 0;
+    while (index < arrayColection.Length)
+    {
+        Console.WriteLine($"Данные массива в {index + 1} ячейке = {arrayColection[index]}");
+        index++;
+    }
+}
+void ChangeArray(int[] arrayColection)
+{
+    int index = 0;
+    int sum = 0;
+    while (index < arrayColection.Length)
+    {
+        sum = sum + arrayColection[index];
+        index++;
+    }
+    Console.WriteLine($"Сумма чисел массива = {sum}");
+}
+
+int[] array = new int[12];
+FillArray(array);
+PrintArray(array);
+ChangeArray(array);
---
+++ Task_03/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу замену элементов массива на противоположные.
+
+void FillArray(int[] arrayColection)
+{
+    int index = 0;
+    while (index < arrayColection.Length)
+    {
+        arrayColection[index] = new Random().Next(-100, 101);
+        Console.Write($" {arrayColection[index]}");
+        index++;
+    }
+}
+
+void PrintArray(int[] arrayColection)
+{
+    int index = 0;
+    while (index < arrayColection.Length)
+    {
+        arrayColection[index] = arrayColection[index] * -1;
+        Console.Write($" {arrayColection[index]}");
+        index++;
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
